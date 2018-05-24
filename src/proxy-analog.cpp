01dc80ddadfe6ac23505b99ade629e8fcde58b8b
Dan Andersson <andedan@student.chalmers.se>
2018-05-24 11:43:00 +0200

    Added messages for execeptions

--- src/proxy-analog.cpp
+++ src/proxy-analog.cpp
@@ -58,46 +58,49 @@ void Analog::body(cluon::OD4Session &od4)
     if(m_debug) {
           std::cout << "[PROXY_ANALOG-PARSED] ";
     }
-    
-    for (std::pair<uint16_t, float> const& pair : reading) {
-      std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
-      cluon::data::TimeStamp sampleTime = cluon::time::convert(tp);
-      int16_t senderStamp = (int16_t) pair.first + m_senderStampOffsetAnalog;
+    try {
+      for (std::pair<uint16_t, float> const& pair : reading) {
+        std::chrono::system_clock::time_point tp = std::chrono::system_clock::now();
+        cluon::data::TimeStamp sampleTime = cluon::time::convert(tp);
+        int16_t senderStamp = (int16_t) pair.first + m_senderStampOffsetAnalog;
 
-      float value;
+        float value;
 
-      if(pair.first == m_analogPinSteerPosition){
-        value = pair.second/((float) m_analogConvSteerPosition)-((float) m_analogOffsetSteerPosition);
-      }else if(pair.first == m_analogPinEbsLine){
-        value = pair.second/((float) m_analogConvEbsLine)-((float) m_analogOffsetEbsLine);
-      }else if(pair.first == m_analogPinServiceTank){
-        value = pair.second/((float) m_analogConvServiceTank)-((float) m_analogOffsetServiceTank);
-      }else if(pair.first == m_analogPinEbsActuator){
-        value = pair.second/((float) m_analogConvEbsActuator)-((float) m_analogOffsetEbsActuator);
-      }else if(pair.first == m_analogPinPressureReg){
-        value = pair.second/((float) m_analogConvPressureReg)-((float) m_analogOffsetPressureReg);
-      }else if(pair.first == m_analogPinSteerPositionRack){
-        value = pair.second/((float) m_analogConvSteerPositionRack)-((float) m_analogOffsetSteerPositionRack);
+        if(pair.first == m_analogPinSteerPosition){
+          value = pair.second/((float) m_analogConvSteerPosition)-((float) m_analogOffsetSteerPosition);
+        }else if(pair.first == m_analogPinEbsLine){
+          value = pair.second/((float) m_analogConvEbsLine)-((float) m_analogOffsetEbsLine);
+        }else if(pair.first == m_analogPinServiceTank){
+          value = pair.second/((float) m_analogConvServiceTank)-((float) m_analogOffsetServiceTank);
+        }else if(pair.first == m_analogPinEbsActuator){
+          value = pair.second/((float) m_analogConvEbsActuator)-((float) m_analogOffsetEbsActuator);
+        }else if(pair.first == m_analogPinPressureReg){
+          value = pair.second/((float) m_analogConvPressureReg)-((float) m_analogOffsetPressureReg);
+        }else if(pair.first == m_analogPinSteerPositionRack){
+          value = pair.second/((float) m_analogConvSteerPositionRack)-((float) m_analogOffsetSteerPositionRack);
+        }
+
+        if(pair.first == m_analogPinSteerPosition || pair.first == m_analogPinSteerPositionRack){
+          opendlv::proxy::GroundSteeringReading msgSteer;
+          msgSteer.groundSteering(value);
+          od4.send(msgSteer, sampleTime, senderStamp);
+        }else if(pair.first == m_analogPinEbsLine || pair.first == m_analogPinServiceTank || pair.first == m_analogPinEbsActuator || pair.first == m_analogPinPressureReg){
+          opendlv::proxy::PressureReading msgPressure;
+          msgPressure.pressure(value);
+          od4.send(msgPressure, sampleTime, senderStamp);
+        }else{
+          opendlv::proxy::VoltageReading msg;
+          msg.torque(pair.second);
+          od4.send(msg, sampleTime, senderStamp);
+        }
+
+        if(m_debug) {
+          std::cout << "Pin " << pair.first << ": " << value << " ";
+        }
+
       }
-
-      if(pair.first == m_analogPinSteerPosition || pair.first == m_analogPinSteerPositionRack){
-        opendlv::proxy::GroundSteeringReading msgSteer;
-        msgSteer.groundSteering(value);
-        od4.send(msgSteer, sampleTime, senderStamp);
-      }else if(pair.first == m_analogPinEbsLine || pair.first == m_analogPinServiceTank || pair.first == m_analogPinEbsActuator || pair.first == m_analogPinPressureReg){
-        opendlv::proxy::PressureReading msgPressure;
-        msgPressure.pressure(value);
-        od4.send(msgPressure, sampleTime, senderStamp);
-      }else{
-        opendlv::proxy::VoltageReading msg;
-        msg.torque(pair.second);
-        od4.send(msg, sampleTime, senderStamp);
-      }
-
-      if(m_debug) {
-        std::cout << "Pin " << pair.first << ": " << value << " ";
-      }
-
+    } catch (...) {
+      std::cerr << "[PROXY_ANALOG_EXCEPT] Exception in parsing" << std::endl;
     }
 
     if(m_debug) {
@@ -121,22 +124,26 @@ void Analog::tearDown()
 }
 
 std::vector<std::pair<uint16_t, float>> Analog::getReadings() {
-  std::vector<std::pair<uint16_t, float>> reading;
-  for(uint16_t const pin : m_pins) {
-    std::string filename = "/sys/bus/iio/devices/iio:device0/in_voltage" 
-        + std::to_string(pin) + "_raw";
-    std::ifstream file(filename, std::ifstream::in);
-    std::string line;
-    if(file.is_open()){
-      std::getline(file, line);
-      uint16_t rawReading = std::stoi(line);
-      reading.push_back(std::make_pair(pin, rawReading*m_conversionConst));
-    } else {
-      std::cerr << "[PROXY_ANALOG] Could not read from analog input. (pin: " << pin 
-          << ", filename: " << filename << ")" << std::endl;
-      reading.push_back(std::make_pair(pin, 0));
+  try{
+    std::vector<std::pair<uint16_t, float>> reading;
+    for(uint16_t const pin : m_pins) {
+      std::string filename = "/sys/bus/iio/devices/iio:device0/in_voltage" 
+          + std::to_string(pin) + "_raw";
+      std::ifstream file(filename, std::ifstream::in);
+      std::string line;
+      if(file.is_open()){
+        std::getline(file, line);
+        uint16_t rawReading = std::stoi(line);
+        reading.push_back(std::make_pair(pin, rawReading*m_conversionConst));
+      } else {
+        std::cerr << "[PROXY_ANALOG] Could not read from analog input. (pin: " << pin 
+            << ", filename: " << filename << ")" << std::endl;
+        reading.push_back(std::make_pair(pin, 0));
+      }
+      file.close();
     }
-    file.close();
+  } catch (...) {
+    std::cerr << "[PROXY_ANALOG_EXCEPT] Exception in getReadings" << std::endl;
   }
   return reading;
 }
